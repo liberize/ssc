3eb56f515b7a6a612e6b38b7d82dd8a5a04b5494
liberize <liberize@gmail.com>
2021-01-01 12:12:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple Shell Script Compiler
+
+this is a simple tool to turn bash script to binary, inspired by the famous shc.
+
+sshc itself is not a compiler such as cc, it rather encodes a shell script and generates C++ source code. It then uses the system compiler to compile a binary which behaves exactly like the original script. Upon execution, the compiled binary will deobfuscate and pipe the script code to child bash process to execute.
+
+# Usage
+
+install gettext, g++ (5.2 or above)
+
+```bash
+sshc script.sh binary
+```
+
+# Features and Limitations
+
+* very simple, easy to read
+* support bash only for now
+* simple code protection with compile time script obfuscation rather than encryption
+* no anti-debugging features
+* pipes shell code to bash to avoid command line exposure
+* support parameters
+* doesn't support script output for now
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/prctl.h>
+
+#include <vector>
+#include "obfuscator.hpp"
+
+auto g_script = OBF(R"(
+${SCRIPT_CONTENT}
+)");
+
+int main(int argc, char** argv)
+{
+    int fd1[2];
+    if (pipe(fd1)==-1)
+    {
+        perror("create pipe failed");
+        return 1;
+    }
+
+    auto p = fork();
+    if (p < 0)
+    {
+        perror("fork failed");
+        return 1;
+    }
+    // parent process
+    else if (p > 0)
+    {
+        close(fd1[0]);  // close reading end of pipe
+
+        // write script content and close writing end of pipe
+        write(fd1[1], g_script.data(), g_script.size());
+        close(fd1[1]);
+
+        // wait for child to exit
+        wait(NULL);
+    }
+    // child process
+    else
+    {
+        // kill child after parent dies
+        prctl(PR_SET_PDEATHSIG, SIGHUP);
+
+        close(fd1[1]);  // close writing end of pipe
+        dup2(fd1[0], 0);
+        close(fd1[0]);
+
+        std::vector<const char*> args(argc + 2);
+        args[0] = "bash";
+        args[1] = "-s";
+        for (auto i = 1; i < argc; i++) {
+            args[i + 1] = argv[i];
+        }
+        args[argc + 1] = NULL;
+        execvp(args[0], (char* const*)args.data());
+        // error in execvp
+        perror("execvp failed");
+        exit(1);
+    }
+}
