a26c9c4d1b7f409151cf83edf1d4467f35554567
liberize <liberize@gmail.com>
2024-12-09 22:04:38 +0800

    format code

--- src/obfuscate.h
+++ src/obfuscate.h
@@ -11,15 +11,15 @@ Usage:
 Pass string literals into the AY_OBFUSCATE macro to obfuscate them at compile
 time. AY_OBFUSCATE returns a reference to an ay::obfuscated_data object with the
 following traits:
-	- Guaranteed obfuscation of string
-	The passed string is encrypted with a simple XOR cipher at compile-time to
-	prevent it being viewable in the binary image
-	- Global lifetime
-	The actual instantiation of the ay::obfuscated_data takes place inside a
-	lambda as a function level static
-	- Implicitly convertable to a char*
-	This means that you can pass it directly into functions that would normally
-	take a char* or a const char*
+    - Guaranteed obfuscation of string
+    The passed string is encrypted with a simple XOR cipher at compile-time to
+    prevent it being viewable in the binary image
+    - Global lifetime
+    The actual instantiation of the ay::obfuscated_data takes place inside a
+    lambda as a function level static
+    - Implicitly convertable to a char*
+    This means that you can pass it directly into functions that would normally
+    take a char* or a const char*
 
 Example:
 const char* obfuscated_string = AY_OBFUSCATE("Hello World");
@@ -29,120 +29,101 @@ std::cout << obfuscated_string << std::endl;
 #pragma once
 #include <cstddef>
 
-namespace ay
-{
-	// Obfuscates a string at compile time
-	template <std::size_t N, char KEY>
-	class obfuscator
-	{
-	public:
-		// Obfuscates the string 'data' on construction
-		constexpr obfuscator(const char* data)
-		{
-			static_assert(KEY != '\0', "KEY must not be the null character.");
+namespace ay {
 
-			// On construction each of the characters in the string is
-			// obfuscated with an XOR cipher based on KEY
-			for (std::size_t i = 0; i < N; i++)
-			{
-				m_data[i] = data[i] ^ KEY;
-			}
-		}
+// Obfuscates a string at compile time
+template <std::size_t N, char KEY>
+class obfuscator {
+public:
+    // Obfuscates the string 'data' on construction
+    constexpr obfuscator(const char* data) {
+        static_assert(KEY != '\0', "KEY must not be the null character.");
 
-		constexpr const char* getData() const
-		{
-			return &m_data[0];
-		}
+        // On construction each of the characters in the string is
+        // obfuscated with an XOR cipher based on KEY
+        for (std::size_t i = 0; i < N; i++) {
+            m_data[i] = data[i] ^ KEY;
+        }
+    }
 
-		constexpr std::size_t getSize() const
-		{
-			return N;
-		}
+    constexpr const char* getData() const {
+        return &m_data[0];
+    }
 
-		constexpr char getKey() const
-		{
-			return KEY;
-		}
+    constexpr std::size_t getSize() const {
+        return N;
+    }
 
-	private:
+    constexpr char getKey() const {
+        return KEY;
+    }
 
-		char m_data[N]{};
-	};
+private:
 
-	// Handles decryption and re-encryption of an encrypted string at runtime
-	template <std::size_t N, char KEY>
-	class obfuscated_data
-	{
-	public:
-		obfuscated_data(const obfuscator<N, KEY>& obfuscator)
-		{
-			for (std::size_t i = 0; i < N; i++)
-			{
-				m_data[i] = obfuscator.getData()[i];
-			}
-		}
+    char m_data[N]{};
+};
 
-		~obfuscated_data()
-		{
-			// Zero m_data to remove it from memory
-			for (std::size_t i = 0; i < N; i++)
-			{
-				m_data[i] = 0;
-			}
-		}
+// Handles decryption and re-encryption of an encrypted string at runtime
+template <std::size_t N, char KEY>
+class obfuscated_data {
+public:
+    obfuscated_data(const obfuscator<N, KEY>& obfuscator) {
+        for (std::size_t i = 0; i < N; i++) {
+            m_data[i] = obfuscator.getData()[i];
+        }
+    }
 
-		// Returns a pointer to the plain text string, decrypting it if
-		// necessary
-		operator char*()
-		{
-			decrypt();
-			return m_data;
-		}
+    ~obfuscated_data() {
+        // Zero m_data to remove it from memory
+        for (std::size_t i = 0; i < N; i++) {
+            m_data[i] = 0;
+        }
+    }
 
-		// Manually decrypt the string
-		void decrypt()
-		{
-			if (is_encrypted())
-			{
-				for (std::size_t i = 0; i < N; i++)
-				{
-					m_data[i] ^= KEY;
-				}
-			}
-		}
+    // Returns a pointer to the plain text string, decrypting it if
+    // necessary
+    operator char*() {
+        decrypt();
+        return m_data;
+    }
 
-		// Manually re-encrypt the string
-		void encrypt()
-		{
-			if (!is_encrypted())
-			{
-				for (std::size_t i = 0; i < N; i++)
-				{
-					m_data[i] ^= KEY;
-				}
-			}
-		}
+    // Manually decrypt the string
+    void decrypt() {
+        if (is_encrypted()) {
+            for (std::size_t i = 0; i < N; i++) {
+                m_data[i] ^= KEY;
+            }
+        }
+    }
 
-		// Returns true if this string is currently encrypted, false otherwise.
-		bool is_encrypted() const
-		{
-			return m_data[N - 1] != '\0';
-		}
+    // Manually re-encrypt the string
+    void encrypt() {
+        if (!is_encrypted()) {
+            for (std::size_t i = 0; i < N; i++) {
+                m_data[i] ^= KEY;
+            }
+        }
+    }
 
-	private:
+    // Returns true if this string is currently encrypted, false otherwise.
+    bool is_encrypted() const {
+        return m_data[N - 1] != '\0';
+    }
 
-		// Local storage for the string. Call is_encrypted() to check whether or
-		// not the string is currently obfuscated.
-		char m_data[N];
-	};
+private:
 
-	// This function exists purely to extract the number of elements 'N' in the
-	// array 'data'
-	template <std::size_t N, char KEY = '.'>
-	constexpr auto make_obfuscator(const char(&data)[N])
-	{
-		return obfuscator<N, KEY>(data);
-	}
+    // Local storage for the string. Call is_encrypted() to check whether or
+    // not the string is currently obfuscated.
+    char m_data[N];
+};
+
+// This function exists purely to extract the number of elements 'N' in the
+// array 'data'
+template <std::size_t N, char KEY = '.'>
+constexpr auto make_obfuscator(const char(&data)[N]) {
+    return obfuscator<N, KEY>(data);
+}
+
 }
 
 // Obfuscates the string 'data' at compile-time and returns a reference to a
@@ -155,14 +136,14 @@ namespace ay
 // functions for decrypting the string and is also implicitly convertable to a
 // char*
 #define AY_OBFUSCATE_KEY(data, key) \
-	[]() -> ay::obfuscated_data<sizeof(data)/sizeof(data[0]), key>& { \
-		constexpr auto n = sizeof(data)/sizeof(data[0]); \
-		static_assert(data[n - 1] == '\0', "String must be null terminated"); \
-		constexpr auto obfuscator = ay::make_obfuscator<n, key>(data); \
-		static auto obfuscated_data = ay::obfuscated_data<n, key>(obfuscator); \
-		return obfuscated_data; \
-	}()
-	
+    []() -> ay::obfuscated_data<sizeof(data)/sizeof(data[0]), key>& { \
+        constexpr auto n = sizeof(data)/sizeof(data[0]); \
+        static_assert(data[n - 1] == '\0', "String must be null terminated"); \
+        constexpr auto obfuscator = ay::make_obfuscator<n, key>(data); \
+        static auto obfuscated_data = ay::obfuscated_data<n, key>(obfuscator); \
+        return obfuscated_data; \
+    }()
+    
 /* -------------------------------- LICENSE ------------------------------------
 
 Public Domain (http://www.unlicense.org)
